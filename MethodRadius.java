f978111b605393c41bec1b39d274bbe02285124e
sumitpagrawal <38112079+sumitpagrawal@users.noreply.github.com>
2018-04-27 16:19:31 +0530

    Add files via upload

---
+++ MethodRadius.java
@@ -0,0 +1,14 @@
+class MethodRadius
+{
+	public static void main(String[] args)
+	{
+	 double value = areaOfCircle(2.12);
+	 System.out.println("AREA OF CIRCLE IS:"+value);	 
+	}
+
+	static double areaOfCircle(double rad)
+	{
+	 double value= (3.14*rad*rad);
+	 return value;	
+	}
+}
