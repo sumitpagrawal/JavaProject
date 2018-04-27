80302c73245b2067a1612161b920ce93fd80079d
sumitpagrawal <38112079+sumitpagrawal@users.noreply.github.com>
2018-04-27 15:14:28 +0530

    Add files via upload

---
+++ UnaryDemo1.java
@@ -0,0 +1,18 @@
+class UnaryDemo1
+{
+
+	public static void main(String[] args)
+	{
+	
+	int x=5;
+	int y;
+	
+	y=++x;
+
+	System.out.println("X VALUE IS:" +x);
+	System.out.println("Y VALUE IS:" +y);
+
+	}
+
+
+}
