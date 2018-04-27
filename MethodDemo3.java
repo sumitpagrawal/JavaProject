557c0e6dd08dca3c23938ad96ace89a203d76bd6
sumitpagrawal <38112079+sumitpagrawal@users.noreply.github.com>
2018-04-27 16:22:50 +0530

    Add files via upload

---
+++ MethodDemo3.java
@@ -0,0 +1,14 @@
+class MethodDemo3
+{
+	static void addition(int no1, int no2)
+	{
+		int c=no1+no2;
+		System.out.println("C VALUE IS:"+c);
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println("MAIN METHOD");
+		addition(50,60);
+	}
+}
