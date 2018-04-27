a092120b41fd05ff9a830e262d7f23d960a773fe
sumitpagrawal <38112079+sumitpagrawal@users.noreply.github.com>
2018-04-27 16:36:13 +0530

    Add files via upload

---
+++ MethodRadius1.java
@@ -0,0 +1,13 @@
+class MethodRadius1
+{
+	static void area(double rad)
+	{
+	double a = 3.14*rad*rad;
+	System.out.println("Area is:"+a);
+	}
+
+	public static void main(String[] args)
+	{
+	area(2);
+	}
+}
