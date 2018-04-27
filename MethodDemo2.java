ee8608af9beb16431670c77ac05c1219743df16f
sumitpagrawal <38112079+sumitpagrawal@users.noreply.github.com>
2018-04-27 15:31:43 +0530

    Add files via upload

---
+++ MethodDemo2.java
@@ -0,0 +1,16 @@
+class MethodDemo2
+{
+
+	public static void main (String[] args)
+	{
+		System.out.println("This is MAIN Method");
+		//test(); 
+	
+	}
+	
+	static void test()
+	{
+		System.out.println("This is TEST Method");
+	}
+
+}
