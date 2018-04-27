153816facbd8ec59cfe7c5cf3541217686f95e81
sumitpagrawal <38112079+sumitpagrawal@users.noreply.github.com>
2018-04-27 16:01:32 +0530

    Add files via upload

---
+++ Method_ReturnType.java
@@ -0,0 +1,16 @@
+class Method_ReturnType
+{
+
+	public static void main(String[] args)
+	{
+		int a=addition();
+		System.out.println("ADDITION IS:"+a);
+	
+	}
+
+	static int addition()
+	{
+		int c=20+30;
+		return c;
+	}
+}
