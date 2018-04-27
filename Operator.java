ba52d52a2c9fad1bbee89510ad58050c5d3b5d8c
sumitpagrawal <38112079+sumitpagrawal@users.noreply.github.com>
2018-04-27 12:38:36 +0530

    Add files via upload

---
+++ Operator.java
@@ -0,0 +1,18 @@
+class Operator
+{
+	public static void main(String[] args)
+	{
+	
+		System.out.println(20+20);
+	    System.out.println(20+60.33);
+		System.out.println(30.99+50.88);
+		System.out.println(10+"java");
+		
+		System.out.println(10+30+"JAVA");
+		System.out.println("java"+20+30); 
+		System.out.println("java"+(20+30));
+		System.out.println('A' + 'B');
+		System.out.println('A' + 20);
+	    System.out.println('A' + "JAVA");		
+	}
+}
