fcad3fc2ab6badc8de625d0b23fda446b8ac289a
sumitpagrawal <38112079+sumitpagrawal@users.noreply.github.com>
2018-06-04 00:40:39 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,22 @@
+package Variables;
+
+public class Employee {
+
+	public static void main(String[] args) {
+	
+		int empId;
+		double empSal;
+		char empGrade;
+		String empName;
+		
+		empId=101;
+		empSal=20000.36;
+		empGrade='A';
+		empName="Dinesh";
+
+		System.out.println("Employee Id is: "+empId);
+		System.out.println("Employee Salary is: "+empSal);
+		System.out.println("Employee Grade is: "+empGrade);
+		System.out.println("Employee Name is: "+empName);
+	}
+}
